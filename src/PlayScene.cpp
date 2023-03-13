f335466b620f89bff90432a977b02777e0a9c7f2
masterbillwoo <91901581+justinmahgeorgebrownyes@users.noreply.github.com>
2023-03-13 16:30:56 -0400

    play scene cleanup

--- src/PlayScene.cpp
+++ src/PlayScene.cpp
@@ -54,52 +54,7 @@ void PlayScene::HandleEvents()
 	}
 }
 
-template <typename T>
-void PlayScene::m_addObjectToGrid(T*& object, const int col, const int row, const TileStatus status)
-{
-	const auto offset = glm::vec2(Config::TILE_SIZE * 0.5f, Config::TILE_SIZE * 0.5f);
-	object = new T();
-	object->GetTransform()->position = m_getTile(col, row)->GetTransform()->position + offset;
-	object->SetGridPosition(static_cast<float>(col), static_cast<float>(row));
-	m_getTile(col, row)->SetTileStatus(status);
-	AddChild(object);
-	//return object;
-}
 
-
-template <typename T>
-void PlayScene::m_moveGameObject(T*& object, int col, int row, TileStatus status) {
-
-	constexpr auto offset = glm::vec2(Config::TILE_SIZE * 0.5f, Config::TILE_SIZE * 0.5f);
-
-
-	if (m_getTile(object->GetGridPosition())->GetTileStatus() != TileStatus::IMPASSABLE) {
-
-		m_getTile(object->GetGridPosition())->SetTileStatus(TileStatus::UNVISITED);
-		m_updateTileMap(object->GetGridPosition(), TileStatus::UNVISITED);
-
-	}
-
-	m_pTarget->GetTransform()->position =
-		m_getTile(col,row)->GetTransform()->position + offset;
-	m_pTarget->SetGridPosition(static_cast<float>(col), static_cast<float>(row));
-
-	if (m_getTile(object->GetGridPosition())->GetTileStatus() != TileStatus::IMPASSABLE) {
-		m_getTile(object->GetGridPosition())->SetTileStatus(status);
-
-		m_updateTileMap(col, row, status);
-
-	}
-
-	if (!m_pPathList.empty()) {
-
-		m_resetPathFinding();
-
-	}
-	//m_computeTileCosts();
-
-
-}
 
 void PlayScene::Start()
 {
@@ -107,18 +62,20 @@ void PlayScene::Start()
 	m_guiTitle = "Play Scene";
 
 
-	m_buildObstacles();
+	//m_buildObstacles();
 
 
+	//game objescts
+	m_pStarShip = new StarShip();
+	AddChild(m_pStarShip);
 
-	m_buildGrid();
+	m_pTarget = new Target();
+	AddChild(m_pTarget);
+
+
+	//m_buildGrid();
 	//auto offset = glm::vec2(Config::TILE_SIZE * 0.5f, Config::TILE_SIZE * 0.5f);
-	m_currentHeuristic = Heuristic::MANHATTAN;
 
-	m_initializeTileMap();
-	m_buildTileMap();
-
-	m_computeTileCosts();
 	//m_findShortestPath();
 	// Add the StarShip to the Scene
 	//m_addObjectToGrid(m_pStarShip, 1, 3, TileStatus::START);
@@ -135,7 +92,7 @@ void PlayScene::Start()
 	SoundManager::Instance().Load("../Assets/Audio/yay.ogg", "yay", SoundType::SOUND_SFX);
 	SoundManager::Instance().Load("../Assets/Audio/thunder.ogg", "thunder", SoundType::SOUND_SFX);
 
-	m_computeTileCosts();
+	//m_computeTileCosts();
 
 	ImGuiWindowFrame::Instance().SetGuiFunction(std::bind(&PlayScene::GUI_Function, this));
 }
@@ -155,70 +112,17 @@ void PlayScene::GUI_Function()
 	ImGui::Separator();
 
 	// Debug Properties
-	static bool toggle_grid = false;
-	if(ImGui::Checkbox("Toggle Grid", &toggle_grid))
+	//static bool toggle_grid = false;
+	if(ImGui::Checkbox("Toggle Grid", &m_isGridEnabled))
 	{
-		m_isGridEnabled = toggle_grid;
-		m_setGridEnabled(m_isGridEnabled);
+		//m_isGridEnabled = toggle_grid;
+		//m_setGridEnabled(m_isGridEnabled);
 	}
 
-	ImGui::Separator();
+	//ImGui::Separator();
 
 
-	if (ImGui::CollapsingHeader("Tile Map")) {
-
-		static char* tile_map[300];
-
-		for (int i = 0; i < m_tileMap.length(); ++i) {
-
-			tile_map[i] = new char[i];
-			sprintf(tile_map[i], "%c + %d", m_tileMap[i], i);
-
-			if ((i % 20) != 0) {
-
-
-				ImGui::SameLine();
-
-			}
-
-			if (m_tileMap[i] == 'S') {
-
-
-				ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.360f, 0.721f, 0.360f, 1.0f)); //green
-
-
-			}
-			else if (m_tileMap[i] == 'G') {
-
-				ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.850f, 0.325f, 0.309f, 1.0f)); //red
-
-
-			}
-			else {
-
-				ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.007f, 0.458f, 0.847f, 1.0f)); //blue
-
-			}
-
-			if (ImGui::Button(tile_map[i], ImVec2(15, 15)))
-			{
-				if (m_tileMap[i] != 'S' && m_tileMap[i] != 'G')
-				{
-					m_tileMap[i] = (m_tileMap[i] == '-') ? 'I' : '-';
-					m_removeAllObstacles();
-					m_buildObstacles();
-					m_buildTileMap();
-					m_resetPathFinding();
-				}
-			}
-
-			ImGui::PopStyleColor();
-
-
-		}
-
-	}
-
+	
 
 
 
@@ -228,74 +132,25 @@ void PlayScene::GUI_Function()
 
 	ImGui::Separator();
 
-	static int radio = static_cast<int>(m_currentHeuristic);
-	ImGui::Text("Heuristic Type");
-	ImGui::RadioButton("Manhattan", &radio, static_cast<int>(Heuristic::MANHATTAN));
-	ImGui::SameLine();
-	ImGui::RadioButton("Euclidean", &radio, static_cast<int>(Heuristic::EUCLIDEAN));
-
-	// check if  current heuristic is not the same as the selection
-	if(m_currentHeuristic != static_cast<Heuristic>(radio))
-	{
-		m_currentHeuristic = static_cast<Heuristic>(radio);
-
-		if (!m_pPathList.empty()) {
-
-			m_resetPathFinding();
-
-		}
-
-
-		m_computeTileCosts();
-	}
+	
 
 	ImGui::Separator();
 
 
-	if (ImGui::Button("find shortest path")) {
-
-		if (!m_pPathList.empty()) {
-
-			m_resetPathFinding();
-
-		}
-		m_findShortestPath();
-
-
-	}
-
-	if (m_pathFound) {
-		ImGui::SameLine();
-		ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Path Found! - Length: %d", (m_pPathList.size() - 1));
-	}
-
+	
 	ImGui::Separator();
 
 	//position variable
-	static int start_position[2] = {
-	static_cast<int>(m_pStarShip->GetGridPosition().x),
-	static_cast<int>(m_pStarShip->GetGridPosition().y) };
+	static int start_position[] = {
+	static_cast<int>(m_pStarShip->GetTransform()->position.x),
+	static_cast<int>(m_pStarShip->GetTransform()->position.y) };
 
-	static int goal_position[2] = {
-	static_cast<int>(m_pTarget->GetGridPosition().x),
-	static_cast<int>(m_pTarget->GetGridPosition().y) };
+	static int goal_position[] = {
+	static_cast<int>(m_pTarget->GetTransform()->position.x),
+	static_cast<int>(m_pTarget->GetTransform()->position.y) };
 
 
-	if (ImGui::Button("Reset Path Finding")) {
-
-		m_resetPathFinding();
-
-	}
-
-	ImGui::SameLine();
-	if (ImGui::Button("Reset simulation")) {
-		m_resetSimulation();
-		start_position[0] = static_cast<int>(m_pStarShip->GetGridPosition().x);
-		start_position[1] = static_cast<int>(m_pStarShip->GetGridPosition().y);
-		start_position[0] = static_cast<int>(m_pTarget->GetGridPosition().x);
-		start_position[1] = static_cast<int>(m_pTarget->GetGridPosition().y);
-
-	}
+	
 
 
 	ImGui::Separator();
@@ -304,16 +159,15 @@ void PlayScene::GUI_Function()
 
 	// StarShip Properties
 
-	if(ImGui::SliderInt2("Start Position", start_position, 0, Config::COL_NUM - 1))
+	if(ImGui::SliderInt2("starship Position", start_position, 0, 800))
 	{
 		// constrain the object within max rows
-		if(start_position[1] > Config::ROW_NUM - 1)
-		{
-			start_position[1] = Config::ROW_NUM - 1;
-		}
+	
+		m_pStarShip->GetTransform()->position.x = start_position[0];
+		m_pStarShip->GetTransform()->position.y = start_position[1];
 
 
-		m_moveGameObject(m_pTarget, start_position[0], start_position[1], TileStatus::GOAL);
+		//m_moveGameObject(m_pTarget, start_position[0], start_position[1], TileStatus::GOAL);
 
 
 	}
@@ -322,18 +176,12 @@ void PlayScene::GUI_Function()
 
 	// Target Properties
 
-	if (ImGui::SliderInt2("Goal Position", goal_position, 0, Config::COL_NUM - 1))
+	if (ImGui::SliderInt2("target Position", goal_position, 0, 800))
 	{
 		// constrain the object within max rows
-		if (goal_position[1] > Config::ROW_NUM - 1)
-		{
-			goal_position[1] = Config::ROW_NUM - 1;
-		}
-
-
-		m_moveGameObject(m_pTarget, goal_position[0], goal_position[1], TileStatus::GOAL);
-
-		m_computeTileCosts();
+		
+		m_pTarget->GetTransform()->position.x = goal_position[0];
+		m_pTarget->GetTransform()->position.y = goal_position[1];
 	}
 
 	ImGui::End();
@@ -348,113 +196,13 @@ void PlayScene::m_buildGrid()
 	{
 		for (int col = 0; col < Config::COL_NUM; ++col)
 		{
-			Tile* tile = new Tile();
-			// world space position
-			tile->GetTransform()->position = glm::vec2(col * tile_size, row * tile_size);
-			tile->SetGridPosition(col, row); // Grid Position
-			tile->SetParent(this);
-			tile->AddLabels();
-			AddChild(tile);
-			tile->SetEnabled(false);
-			m_pGrid.push_back(tile);
-		}
-	}
 
-	// setup the neighbour references for each tile in the grid
-	// tiles = nodes in our graph
-
-	for (int row = 0; row < Config::ROW_NUM; ++row)
-	{
-		for (int col = 0; col < Config::COL_NUM; ++col)
-		{
-			Tile* tile = m_getTile(col, row);
-
-			// TopMost Neighbour
-			if(row == 0)
-			{
-				tile->SetNeighbourTile(NeighbourTile::TOP_TILE, nullptr);
-			}
-			else
-			{
-				tile->SetNeighbourTile(NeighbourTile::TOP_TILE, m_getTile(col, row - 1));
-			}
-
-			// RightMost Neighbour
-			if(col == Config::COL_NUM - 1)
-			{
-				tile->SetNeighbourTile(NeighbourTile::RIGHT_TILE, nullptr);
-			}
-			else
-			{
-				tile->SetNeighbourTile(NeighbourTile::RIGHT_TILE, m_getTile(col + 1,row));
-			}
-
-			// BottomMost Neighbour
-			if(row == Config::ROW_NUM - 1)
-			{
-				tile->SetNeighbourTile(NeighbourTile::BOTTOM_TILE, nullptr);
-			}
-			else
-			{
-				tile->SetNeighbourTile(NeighbourTile::BOTTOM_TILE, m_getTile(col, row + 1));
-			}
-
-			// LeftMost Neighbour
-			if (col == 0)
-			{
-				tile->SetNeighbourTile(NeighbourTile::LEFT_TILE, nullptr);
-			}
-			else
-			{
-				tile->SetNeighbourTile(NeighbourTile::LEFT_TILE, m_getTile(col - 1, row));
-			}
 		}
 	}
 }
 
-bool PlayScene::m_getGridEnabled() const
-{
-	return m_isGridEnabled;
-}
 
-void PlayScene::m_setGridEnabled(const bool state)
-{
-	m_isGridEnabled = state;
 
-	for (const auto tile : m_pGrid)
-	{
-		tile->SetEnabled(m_isGridEnabled); // enables each tile object
-		tile->SetLabelsEnabled(m_isGridEnabled); // enables the corresponding labels
-	}
-}
-
-void PlayScene::m_computeTileCosts()
-{
-	float distance = 0.0f;
-	float dx = 0.0f;
-	float dy = 0.0f;
-
-	// for each tile in the grid, loop
-	for (const auto tile : m_pGrid)
-	{
-		// compute the distance from each tile to the goal tile
-		// distance (f) = tile cost (g) + heuristic estimate (h)
-		switch(m_currentHeuristic)
-		{
-		case Heuristic::MANHATTAN:
-			dx = abs(tile->GetGridPosition().x - m_pTarget->GetGridPosition().x);
-			dy = abs(tile->GetGridPosition().y - m_pTarget->GetGridPosition().y);
-			distance = dx + dy;
-			break;
-		case Heuristic::EUCLIDEAN:
-			// calculates the euclidean distance ("as the crow flies") for each tile
-			distance = Util::Distance(tile->GetGridPosition(), m_pTarget->GetGridPosition());
-			break;
-		}
-
-		tile->SetTileCost(distance);
-	}
-}
 
 void PlayScene::m_buildObstacles() {
 
@@ -465,282 +213,4 @@ void PlayScene::m_buildObstacles() {
 
 }
 
-void PlayScene::m_removeObstacleAt(int col, int row)
-{
 
-	for (auto& obstacle : m_pObstacles) {
-
-		if (obstacle != nullptr) {
-
-
-			if (static_cast<int>(obstacle->GetGridPosition().x) == col && static_cast<int>(obstacle->GetGridPosition().y) == row) {
-				RemoveChild(obstacle);
-				m_pObstacles[(row * Config::COL_NUM) + col] = new Obstacle();
-			}
-
-		}
-
-	}
-
-
-}
-
-void PlayScene::m_removeObstacleAt(glm::vec2 grid_position)
-{
-	m_removeObstacleAt(static_cast<int>(grid_position.x), static_cast<int>(grid_position.y));
-
-
-
-}
-
-void PlayScene::m_removeAllObstacles()
-{
-	for (auto& obstacle : m_pObstacles) {
-
-		RemoveChild(obstacle);
-	}
-	m_pObstacles.clear();
-
-
-}
-
-void PlayScene::m_findShortestPath()
-{
-	m_getTile(m_pStarShip->GetGridPosition())->SetTileParent(nullptr);
-	m_pOpenList.push(m_getTile(m_pStarShip->GetGridPosition()));
-	bool goal_found = false;
-
-	while (!m_pOpenList.empty() && !goal_found) {
-
-		const auto current_node = m_pOpenList.top();
-		std::cout << current_node->GetGridPosition().x << ", " << current_node->GetGridPosition().y << std::endl;
-
-		m_pOpenList.pop();
-		current_node->SetTileStatus(TileStatus::CLOSED);
-
-		for (auto neighbour : current_node->GetNeighbourTiles()) {
-
-			if (neighbour == nullptr || neighbour->GetTileStatus() == TileStatus::IMPASSABLE) {
-
-				continue;
-
-
-			}
-			if (neighbour->GetTileStatus() == TileStatus::GOAL) {
-
-				goal_found = true;
-				neighbour->SetTileParent(current_node);
-				std::cout << "Goal Found at: " << neighbour->GetGridPosition().x << ", " << neighbour->GetGridPosition().y << std::endl;
-				m_pPathList.push_front(neighbour);
-				break;
-
-
-
-			}
-			if (neighbour->GetTileStatus() == TileStatus::UNVISITED) {
-
-				neighbour->SetTileParent(current_node);
-				neighbour->SetTileStatus(TileStatus::OPEN);
-				m_pOpenList.push(neighbour);
-
-			}
-
-
-		}
-
-
-
-	}
-
-	if (goal_found) {
-
-		m_pathFound = true;
-		m_buildPathList();
-		m_displayPathList();
-	}
-	else {
-
-		const auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
-		SetConsoleTextAttribute(handle, 4); // red
-		std::cout << "No Path Found!" << std::endl;
-		SetConsoleTextAttribute(handle, 15); // red
-
-	}
-
-
-}
-
-void PlayScene::m_buildPathList()
-{
-	while (m_pPathList.front()->GetTileParent() != nullptr) {
-
-		m_pPathList.push_front(m_pPathList.front()->GetTileParent());
-
-	}
-
-
-}
-
-void PlayScene::m_displayPathList()
-{
-	std::cout << "Total Path Length is: " << m_pPathList.size() - 1 << std:: endl;
-	std::cout << "Path LIst is :" << std::endl;
-	std::cout << "===============" << std::endl;
-	for (const auto node : m_pPathList) {
-		node->SetTileStatus(TileStatus::PATH);
-		std::cout << node->GetGridPosition().x << ", " << node->GetGridPosition().y << std::endl;
-	}
-
-
-}
-
-void PlayScene::m_resetPathFinding()
-{
-	while (!m_pOpenList.empty()) {
-		m_pOpenList.pop();
-	}
-
-	while (!m_pPathList.empty()) {
-		m_pPathList.pop_front();
-	}
-
-	m_pathFound = false;
-
-	for (const auto tile : m_pGrid) {
-
-		if (tile->GetTileStatus() == TileStatus::OPEN 
-			|| tile->GetTileStatus() == TileStatus::CLOSED 
-			|| tile->GetTileStatus() == TileStatus::PATH) {
-
-			tile->SetTileStatus(TileStatus::UNVISITED);
-
-
-		}
-
-	}
-
-
-	m_getTile(m_pStarShip->GetGridPosition())->SetTileStatus(TileStatus::START);
-	m_getTile(m_pTarget->GetGridPosition())->SetTileStatus(TileStatus::GOAL);
-
-
-}
-
-void PlayScene::m_resetSimulation()
-{
-	m_removeAllObstacles();
-	m_buildObstacles();
-	RemoveChild(m_pStarShip);
-	RemoveChild(m_pTarget);
-	m_initializeTileMap();
-	m_buildTileMap();
-	m_computeTileCosts();
-	m_resetPathFinding();
-
-}
-
-Tile* PlayScene::m_getTile(const int col, const int row) const
-{
-	return m_pGrid[(row * Config::COL_NUM) + col];
-}
-
-Tile* PlayScene::m_getTile(const glm::vec2 grid_position) const
-{
-	const auto col = grid_position.x;
-	const auto row = grid_position.y;
-
-	return m_getTile(static_cast<int>(col), static_cast<int>(row));
-}
-
-void PlayScene::m_initializeTileMap()
-{
-	m_tileMap = "---------I----------";
-	m_tileMap += "---------I----------";
-	m_tileMap += "---------I----------";
-	m_tileMap += "--S------I----------";
-	m_tileMap += "---------I----------";
-	m_tileMap += "---------I----------";
-	m_tileMap += "-----I---I---I------";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I-G----";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I------";
-	m_tileMap += "-----I-------I------";
-
-}
-
-void PlayScene::m_buildTileMap()
-{
-	for (int row = 0; row < Config::ROW_NUM; ++row) {
-		for (int col = 0; col < Config::COL_NUM; ++col) {
-
-			if (m_tileMap[(row * Config::COL_NUM) + col] == 'I') {
-
-				m_addObjectToGrid(m_pObstacles[(row * Config::COL_NUM) + col], col, row, TileStatus::IMPASSABLE);
-			}
-			if (m_tileMap[(row * Config::COL_NUM) + col] == 'S') {
-
-				m_addObjectToGrid(m_pStarShip, col, row, TileStatus::START);
-			}
-			if (m_tileMap[(row * Config::COL_NUM) + col] == 'G') {
-
-				m_addObjectToGrid(m_pTarget, col, row, TileStatus::GOAL);
-			}
-			if (m_tileMap[(row * Config::COL_NUM) + col] == '-') {
-
-				m_getTile(col, row) ->SetTileStatus(TileStatus::UNVISITED);
-				//m_addObjectToGrid(m_pStarShip, col, row, TileStatus::START);
-			}
-			
-		}
-	}
-
-
-}
-
-void PlayScene::m_updateTileMap(int col, int row, TileStatus status)
-{
-
-	switch (status) {
-	case TileStatus::UNVISITED:
-		m_tileMap[(row * Config::COL_NUM) + col] = '-';
-			break;
-	
-	case TileStatus::OPEN:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'O';
-			break;
-	
-	case TileStatus::CLOSED:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'C';
-			break;
-
-	case TileStatus::GOAL:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'G';
-		break;
-
-	case TileStatus::START:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'S';
-		break;
-
-	case TileStatus::IMPASSABLE:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'I';
-		break;
-	
-	case TileStatus::PATH:
-		m_tileMap[(row * Config::COL_NUM) + col] = 'P';
-		break;
-	
-
-	}
-
-}
-
-void PlayScene::m_updateTileMap(glm::vec2 grid_position, TileStatus status)
-{
-
-	m_updateTileMap(static_cast<int>(grid_position.x), static_cast<int>(grid_position.y), status);
-
-}
--- src/PlayScene.h
+++ src/PlayScene.h
@@ -5,11 +5,11 @@
 #include "Scene.h"
 #include "Target.h"
 #include "StarShip.h"
-#include "Tile.h"
-#include "Heuristic.h"
+//#include "Tile.h"
+//#include "Heuristic.h"
 #include "Obstacle.h"
-#include <queue>
-#include "TileComparator.h"
+//#include <queue>
+//#include "TileComparator.h"
 
 class PlayScene : public Scene
 {
@@ -37,53 +37,53 @@ private:
 	bool m_bDebugView;
 
 	// Pathfinding Objects and Functions
-	std::vector<Tile*> m_pGrid;
+	//std::vector<Tile*> m_pGrid;
 	bool m_isGridEnabled;
 
 	void m_buildGrid();
-	bool m_getGridEnabled() const;
-	void m_setGridEnabled(bool state);
-	void m_computeTileCosts();
+	//bool m_getGridEnabled() const;
+	//void m_setGridEnabled(bool state);
+	//void m_computeTileCosts();
 
 	// obstacle list
 	std::vector<Obstacle*> m_pObstacles;
 	void m_buildObstacles();
 
 	//utiltiy
-	void m_removeObstacleAt(int col, int row);
-	void m_removeObstacleAt(glm::vec2 grid_position);
-	void m_removeAllObstacles();
+	//void m_removeObstacleAt(int col, int row);
+	//void m_removeObstacleAt(glm::vec2 grid_position);
+	//void m_removeAllObstacles();
 
 
 	// tile lists for pathfinding
-	std::priority_queue<Tile*, std::vector<Tile*>, TileComparator> m_pOpenList;
-	std::list<Tile*> m_pPathList;
-	bool m_pathFound = false;
+	//std::priority_queue<Tile*, std::vector<Tile*>, TileComparator> m_pOpenList;
+	//std::list<Tile*> m_pPathList;
+	//bool m_pathFound = false;
 
 
 	//std::vector<Tile*> m_pClosedList;
 
 	// calculate shortest path functions
-	void m_findShortestPath();
-	void m_buildPathList();
-	void m_displayPathList();
-	void m_resetPathFinding();
-	void m_resetSimulation();
+	//void m_findShortestPath();
+	//void m_buildPathList();
+	//void m_displayPathList();
+	// m_resetPathFinding();
+	//void m_resetSimulation();
 
 	
 	// TODO: some kind of data structure for the path list
 	// suggestions: a queue data structure or a linked list
 
 	// convenience Functions to convert world to grid space
-	Tile* m_getTile(int col, int row) const;
-	Tile* m_getTile(glm::vec2 grid_position) const;
+	//Tile* m_getTile(int col, int row) const;
+	//Tile* m_getTile(glm::vec2 grid_position) const;
 
-	template <typename T>
-	void m_addObjectToGrid(T*& object, int col, int row, TileStatus status);
+	//template <typename T>
+	//void m_addObjectToGrid(T*& object, int col, int row, TileStatus status);
 
 
-	template <typename T>
-	void m_moveGameObject(T*& object, int col, int row, TileStatus status);
+	//template <typename T>
+	//void m_moveGameObject(T*& object, int col, int row, TileStatus status);
 
 
 
@@ -91,16 +91,16 @@ private:
 	//void m_markImpassable();
 
 	// heuristic
-	Heuristic m_currentHeuristic;
+	//Heuristic m_currentHeuristic;
 
 	//tile map
-	std::string m_tileMap;
-	void m_initializeTileMap();
-	void m_buildTileMap();
+	//std::string m_tileMap;
+	//void m_initializeTileMap();
+	//void m_buildTileMap();
 
-	//convenience functions
-	void m_updateTileMap(int col, int row, TileStatus status);
-	void m_updateTileMap(glm::vec2 grid_position, TileStatus status);
+	////convenience functions
+	//void m_updateTileMap(int col, int row, TileStatus status);
+	//void m_updateTileMap(glm::vec2 grid_position, TileStatus status);
 
 
 };
